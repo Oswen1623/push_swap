b859d701509b8c50e992711ee71b5bae8b61cb07
Lucie Nguyen <lucinguy@f2r10s22.clusters.42paris.fr>
2026-01-14 18:32:22 +0100

    oui

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: lucinguy <lucinguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 15:25:58 by lucinguy          #+#    #+#             */
-/*   Updated: 2026/01/13 17:17:54 by lucinguy         ###   ########.fr       */
+/*   Updated: 2026/01/14 18:32:15 by lucinguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,35 @@
 
 int	main(int argc, char **argv)
 {
-	void	**newstring;
+	char	**newstring;
+	long	*convd;
 	int		i;
-	long	convd[argc - 1];
+	int		size;
 
 	newstring = NULL;
+	convd = NULL;
 	i = 0;
-	convd[0] = 0;
 	if ((argc == 1) || (argc == 2 && !argv[1][0]))
 		return (1);
 	else if (argc == 2)
 	{
 		newstring = ft_split(argv[1], ' ');
-		while (newstring[i++])
-			newstring[i] = ft_atol(newstring[i]);
+		size = 0;
+		while (newstring[size])
+			size++;
+		convd = malloc(sizeof(long) * size);
+		i = 0;
+		while (i < size)
+		{
+			convd[i] = ft_atol(newstring[i]);
+			i++;
+		}
+		free(newstring);
 	}
 	else
-		newstring = multiple_args(argc, **argv);
-	if (!newstring)
-		return (1);
-	free(newstring);
+	{
+		convd = multiple_strings(argc, argv);
+	}
+	free(convd);
 	return (0);
 }
