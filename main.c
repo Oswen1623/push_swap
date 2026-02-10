0747e4eee19f9bec7a05dbb5c8a897000a4f162b
Oswen1623 <lucie.nguyen0610@gmail.com>
2026-02-10 12:28:59 +0100

    oui

--- main.c
+++ main.c
@@ -6,46 +6,58 @@
 /*   By: lucinguy <lucinguy@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 15:25:58 by lucinguy          #+#    #+#             */
-/*   Updated: 2026/02/09 17:23:55 by lucinguy         ###   ########.fr       */
+/*   Updated: 2026/02/10 12:28:16 by lucinguy         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+static void	sort_by_size(t_stack_node **a, t_stack_node **b)
+{
+	int	len;
+
+	if (stack_sorted(*a))
+		return ;
+	len = stack_len(*a);
+	if (len == 2)
+		sa(a, true);
+	else if (len == 3)
+		sort_three(a);
+	else
+		sort_stacks(a, b);
+}
+
+static void	cleanup_split(char **split_args)
+{
+	int	i;
+
+	if (!split_args)
+		return ;
+	i = 0;
+	while (split_args[i])
+		free(split_args[i++]);
+	free(split_args);
+}
 
 int	main(int argc, char **argv)
 {
 	t_stack_node	*a;
 	t_stack_node	*b;
 	char			**split_args;
-	int				i;
 
 	a = NULL;
 	b = NULL;
 	split_args = NULL;
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
 		return (1);
-	else if (argc == 2)
+	if (argc == 2)
 	{
 		split_args = ft_split(argv[1], ' ');
 		argv = split_args - 1;
 	}
 	init_stack_a(&a, argv + 1);
-	if (!stack_sorted(a))
-	{
-		if (stack_len(a) == 2)
-			sa(&a, true);
-		else if (stack_len(a) == 3)
-			sort_three(&a);
-		else
-			sort_stacks(&a, &b);
-	}
+	sort_by_size(&a, &b);
 	free_stack(&a);
-	if (split_args)
-	{
-		i = 0;
-		while (split_args[i])
-			free(split_args[i++]);
-		free(split_args);
-	}
+	cleanup_split(split_args);
 	return (0);
 }
